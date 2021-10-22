2b77fa75bdebba769da7260247fc316576fd0c1c
rahibhassan <raheeb@tridz.com>
2021-10-22 06:37:44 +0000

    Removed Extra workspace and tweaked expense

--- ev_gn/ev_gn/doctype/expense/expense.py
+++ ev_gn/ev_gn/doctype/expense/expense.py
@@ -3,36 +3,40 @@
 
 import frappe
 from frappe.model.document import Document
+from datetime import datetime
 
-# def create_journal_entry(self):
+def create_journal_entry(self):
 
-# 	accounts = {
-# 		"EMI": "EMI - EJ",
-# 		"Service": "Service - EJ",
-# 		"Others": "Other - EJ"
-# 	}
+	accounts = {
+		"EMI": "EMI - EJ",
+		"Service": "Service - EJ",
+		"Others": "Other - EJ",
+		"Salary": "Salary - EJ"
+	}
 
-# 	account = accounts.get("EMI", "nothing")
+	account = accounts.get(self.expense_type, "Other - EJ")
+	account2 = "Cash - EJ"
 
-	
-# 	journal_entry = frappe.get_doc({
-# 			'doctype': 'Journal Entry',
-# 		})
+	journal_entry = frappe.get_doc({
+			'doctype': 'Journal Entry',
+			'posting_date': datetime.today(),
+			"accounts":[
+			{
+				"account": account,
+				"credit_in_account_currency": self.amount
+			},
+			{
+				"account": account2,
+				"debit_in_account_currency": self.amount
+			}
+		]
+		})
 
-# 	journal_entry.append({
-# 		"accounts",{
-# 			"account": account,
-# 			"credit_in_account_currency": self.amount
-# 		},
-# 		"accounts", {
-# 			"account": "EMI - EJ",
-# 			"debit_in_account_currency": self.amount
-# 		}
-# 	})
+	journal_entry.insert()
+	journal_entry.submit()
 
 
 class Expense(Document):
-	pass
-	# def before_submit(self):
-	# 	create_journal_entry(self)
+	def before_submit(self):
+		create_journal_entry(self)
 		
