728743dbed3071f3b50a8e67d74a7b34afe4c05a
rahibhassan <raheeb@tridz.com>
2021-11-10 10:22:00 +0000

    Accounts fix

--- ev_gn/ev_gn/doctype/trip_sheet/trip_sheet.py
+++ ev_gn/ev_gn/doctype/trip_sheet/trip_sheet.py
@@ -37,9 +37,6 @@ def create_purchase_invoice(supplier, site, rate, quantity, amount, trip, date, 
 		"total": trip * amount,
 		"no_of_trips": trip,
 		"vehicle": vehicle,
-		"is_paid" : 1,
-		"mode_of_payment": "Cash",
-		"cash_bank_account": "Cash - EJ",
 		"paid_amount": trip * amount,
 		"trip_id": name
 		})
@@ -131,19 +128,17 @@ class TripSheet(Document):
 			sales_invoice = create_sales_invoice(self, data)
 			data.sales_invoice_id = sales_invoice										
 			purchase_invoice = create_purchase_invoice(data.supplier, data.supplier_site, data.supplier_rate, data.supplier_quantity, data.supplier_amount, data.trip, self.date, data.item, data.uom, self.vehicle, self.name)	
-			data.purchase_invoice_id = purchase_invoice
-			# frc = create_frc(self, data)							
-			# if data.multiple_supplier == 1:																
-			# 	purchase_invoice_partner = create_purchase_invoice(data.supplier_partner, data.supplier_site, data.supplier_partner_rate, data.supplier_partner_quantity, data.supplier_partner_amount, data.trip, self.date, data.item, data.uom, self.vehicle, self.name)
-			# 	data.partner_purchase_invoice_id = purchase_invoice_partner.name
-			# if data.paid_amount:															
-			# 	amount_paid = data.paid_amount
-			# 	payment_mode = data.payment_method
-			# 	payment_entry = create_payment_entry(self, data, sales_invoice.name, amount_paid, payment_mode)
-			# if data.bata_rate:
-			# 	bata_amount = data.bata_rate
-			# elif data.bata_percentage:
-			# 	bata_amount = data.bata_rate
-			# expense = create_expense(bata_amount, data, self)
-			# data.purchase_invoice_id = purchase_invoice.name
-			# data.sales_invoice_id = sales_invoice.name
+			data.purchase_invoice_id = purchase_invoice						
+			if data.multiple_supplier == 1:																
+				purchase_invoice_partner = create_purchase_invoice(data.supplier_partner, data.supplier_site, data.supplier_partner_rate, data.supplier_partner_quantity, data.supplier_partner_amount, data.trip, self.date, data.item, data.uom, self.vehicle, self.name)
+				data.partner_purchase_invoice_id = purchase_invoice_partner
+			frc = create_frc(self, data)
+			if data.paid_amount:															
+				amount_paid = data.paid_amount
+				payment_mode = data.payment_method
+				payment_entry = create_payment_entry(self, data, sales_invoice, amount_paid, payment_mode)
+			if data.bata_rate:
+				bata_amount = data.bata_rate
+			elif data.bata_percentage:
+				bata_amount = data.bata_rate
+			expense = create_expense(bata_amount, data, self)
